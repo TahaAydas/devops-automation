0634e1f7c967498b419281beea5a03c1fbc547c6
Taha <you@example.com>
2022-09-12 05:39:48 +0300

    some changes

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "My First SpringBoot App in Vodafone";
 	}
 
 	public static void main(String[] args) {
